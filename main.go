f94f88abce6f9f30f5e338caf939a72f39794fd8
Dana Ross <dana@danaross.dev>
2020-12-03 13:16:30 -0500

    gofmt

--- main.go
+++ main.go
@@ -1,83 +1,83 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"path"
-	"fmt"
-	"log"
-	"flag"
 	"os"
+	"path"
 	"strings"
 )
 
 var VERSION = "0.0.2"
 
-var upstreamHosts = map[string]string {
+var upstreamHosts = map[string]string{
 	"localhost:8080": "csixty4.com",
 	"localhost:8081": "csixty4.com",
 }
 
 func makeURLAbsolute(r *http.Request, protocol string) string {
 	urlStr := r.URL.String()
-    if u, err := url.Parse(urlStr); err == nil {
-        // If url was relative, make absolute by
-        // combining with request path.
-        // The browser would probably do this for us,
-        // but doing it ourselves is more reliable.
+	if u, err := url.Parse(urlStr); err == nil {
+		// If url was relative, make absolute by
+		// combining with request path.
+		// The browser would probably do this for us,
+		// but doing it ourselves is more reliable.
 
-        // NOTE(rsc): RFC 2616 says that the Location
-        // line must be an absolute URI, like
-        // "http://www.google.com/redirect/",
-        // not a path like "/redirect/".
-        // Unfortunately, we don't know what to
-        // put in the host name section to get the
-        // client to connect to us again, so we can't
-        // know the right absolute URI to send back.
-        // Because of this problem, no one pays attention
-        // to the RFC; they all send back just a new path.
-        // So do we.
-        oldpath := r.URL.Path
-        if oldpath == "" { // should not happen, but avoid a crash if it does
-            oldpath = "/"
-        }
-        if u.Scheme == "" {
-            // no leading http://server
-            if urlStr == "" || urlStr[0] != '/' {
-                // make relative path absolute
-                olddir, _ := path.Split(oldpath)
-                urlStr = olddir + urlStr
-            }
+		// NOTE(rsc): RFC 2616 says that the Location
+		// line must be an absolute URI, like
+		// "http://www.google.com/redirect/",
+		// not a path like "/redirect/".
+		// Unfortunately, we don't know what to
+		// put in the host name section to get the
+		// client to connect to us again, so we can't
+		// know the right absolute URI to send back.
+		// Because of this problem, no one pays attention
+		// to the RFC; they all send back just a new path.
+		// So do we.
+		oldpath := r.URL.Path
+		if oldpath == "" { // should not happen, but avoid a crash if it does
+			oldpath = "/"
+		}
+		if u.Scheme == "" {
+			// no leading http://server
+			if urlStr == "" || urlStr[0] != '/' {
+				// make relative path absolute
+				olddir, _ := path.Split(oldpath)
+				urlStr = olddir + urlStr
+			}
 
-            var query string
-            if i := strings.Index(urlStr, "?"); i != -1 {
-                urlStr, query = urlStr[:i], urlStr[i:]
-            }
+			var query string
+			if i := strings.Index(urlStr, "?"); i != -1 {
+				urlStr, query = urlStr[:i], urlStr[i:]
+			}
 
-            // clean up but preserve trailing slash
-            trailing := strings.HasSuffix(urlStr, "/")
-            urlStr = path.Clean(urlStr)
-            if trailing && !strings.HasSuffix(urlStr, "/") {
-                urlStr += "/"
-            }
+			// clean up but preserve trailing slash
+			trailing := strings.HasSuffix(urlStr, "/")
+			urlStr = path.Clean(urlStr)
+			if trailing && !strings.HasSuffix(urlStr, "/") {
+				urlStr += "/"
+			}
 			urlStr += query
-			
+
 			urlStr = fmt.Sprintf("%s://%s%s", protocol, r.Host, urlStr)
-        }
+		}
 	}
-	
+
 	return urlStr
 }
 
 func RedirectAwareReverseProxy(protocol string, upstreamHosts map[string]string) *httputil.ReverseProxy {
 
 	director := func(req *http.Request) {
-		
-			req.Header.Set("x-tr-originalurl", makeURLAbsolute(req, protocol))
-			req.URL.Host = upstreamHosts[req.Host]
-			req.Host = upstreamHosts[req.Host]
-			req.URL.Scheme = "https"
+
+		req.Header.Set("x-tr-originalurl", makeURLAbsolute(req, protocol))
+		req.URL.Host = upstreamHosts[req.Host]
+		req.Host = upstreamHosts[req.Host]
+		req.URL.Scheme = "https"
 	}
 
 	modifyResponse := func(r *http.Response) error {
@@ -88,7 +88,7 @@ func RedirectAwareReverseProxy(protocol string, upstreamHosts map[string]string)
 			client := &http.Client{
 				CheckRedirect: func(req *http.Request, via []*http.Request) error {
 					return http.ErrUseLastResponse
-			} }
+				}}
 
 			r, err := client.Get(nextURL)
 
@@ -106,11 +106,11 @@ func RedirectAwareReverseProxy(protocol string, upstreamHosts map[string]string)
 		log.Printf("Original: %s Location: %s", originalURL, nextURL)
 		return nil
 	}
-	
+
 	return &httputil.ReverseProxy{
-		Director: director,
+		Director:       director,
 		ModifyResponse: modifyResponse,
-		Transport: NewCachedRoundTripper(10),
+		Transport:      NewCachedRoundTripper(10),
 	}
 
 }
